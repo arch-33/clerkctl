479b771d8036acd4e8be43c3a5f2780c93eb601e
arch-33 <elmousaaif.mohamed@gmail.com>
2026-03-04 14:25:20 +0000

    setup code formatting

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,28 @@
-import { buildApplication, buildRouteMap } from "@stricli/core";
-import { buildInstallCommand, buildUninstallCommand } from "@stricli/auto-complete";
-import { name, version, description } from "../package.json";
-import { subdirCommand } from "./commands/subdir/command";
-import { nestedRoutes } from "./commands/nested/commands";
+import { buildApplication, buildRouteMap } from '@stricli/core';
+import { buildInstallCommand, buildUninstallCommand } from '@stricli/auto-complete';
+import { name, version, description } from '../package.json';
+import { projectRoutes } from './commands/project/commands';
+import { appRoutes } from './commands/app/commands';
 
 const routes = buildRouteMap({
-    routes: {
-        subdir: subdirCommand,
-        nested: nestedRoutes,
-        install: buildInstallCommand("clerk-utils", { bash: "__clerk-utils_bash_complete" }),
-        uninstall: buildUninstallCommand("clerk-utils", { bash: true }),
+  routes: {
+    project: projectRoutes,
+    app: appRoutes,
+    install: buildInstallCommand('clerk-utils', { bash: '__clerk-utils_bash_complete' }),
+    uninstall: buildUninstallCommand('clerk-utils', { bash: true }),
+  },
+  docs: {
+    brief: description,
+    hideRoute: {
+      install: true,
+      uninstall: true,
     },
-    docs: {
-        brief: description,
-        hideRoute: {
-            install: true,
-            uninstall: true,
-        },
-    },
+  },
 });
 
 export const app = buildApplication(routes, {
-    name,
-    versionInfo: {
-        currentVersion: version,
-    },
+  name,
+  versionInfo: {
+    currentVersion: version,
+  },
 });
